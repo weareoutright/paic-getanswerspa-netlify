e218c56886dbc13b089da223fddde1f8c8c4ac96
Dakota Fabro <dakota@weareoutright.com>
2023-10-27 17:01:13 -0400

    Replace pngs with svgs

--- src/constants/paicInfo.js
+++ src/constants/paicInfo.js
@@ -1,5 +1,5 @@
-import logo from "../images/paic-logo.png";
-import chapter from "../images/PAAAP_1.png";
+import logo from "../images/paic-logo.svg";
+import chapter from "../images/PAAAP_1.svg";
 
 export const PAIC_INFO = {
   siteName: "GETANSWERSPA.ORG",
