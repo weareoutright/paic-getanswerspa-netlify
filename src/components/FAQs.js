c8883a9b61696db14c1904c2d3b06adb9fd2585b
Dakota Fabro <dakota@weareoutright.com>
2023-10-27 16:52:07 -0400

    Address whitespace in FAQ section

--- src/components/FAQs.js
+++ src/components/FAQs.js
@@ -7,10 +7,10 @@ const FAQs = () => {
   return (
     <div className="FAQs" id="faqs">
       <div className="faq-header-and-image">
-        <h2>FAQs</h2>
         <img className="faq-image" alt="faq" src={FAQImage} />
       </div>
       <div className="faq-content">
+        <h2>FAQs</h2>
         {TOPICS.map((topic) => {
           return <SingleFAQ key={`${topic.title}-key`} topic={topic} />;
         })}
