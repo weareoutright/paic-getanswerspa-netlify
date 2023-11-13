277c16b7e8fa56655e5600e64706ccd109281c50
Dakota Fabro <dakota@weareoutright.com>
2023-11-13 15:39:40 -0800

    Implement carousel btns on desktop

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -43,19 +43,21 @@ const Quote = () => {
             className="active"
             aria-current="true"
             aria-label="Slide 1"
-          ></button>
+          />
           <button
             type="button"
             data-bs-target="#carouselExampleIndicators"
             data-bs-slide-to="1"
             aria-label="Slide 2"
-          ></button>
+            aria-current="true"
+          />
           <button
             type="button"
             data-bs-target="#carouselExampleIndicators"
             data-bs-slide-to="2"
             aria-label="Slide 3"
-          ></button>
+            aria-current="true"
+          />
         </div>
         <div className="close-quotation">,,</div>
       </div>
