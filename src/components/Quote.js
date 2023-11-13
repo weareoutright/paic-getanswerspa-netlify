4d79ef2b1b932b7ed7ec7fe00c047704bf79f241
Dakota Fabro <dakota@weareoutright.com>
2023-11-13 15:08:44 -0800

    Implement carousel for desktop

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,60 +1,61 @@
 import React, { useState } from "react";
-// import { QUOTES } from "../constants/quotes";
-
-const QUOTES = [
-  {
-    id: 0,
-    content:
-      "0 For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
-  },
-  {
-    id: 1,
-    content:
-      "1 For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
-  },
-  {
-    id: 2,
-    content:
-      "2 For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
-  },
-];
+import { QUOTES } from "../constants/quotesInfo";
 
 const Quote = () => {
-  const [currentQuoteId, setCurrentQuoteId] = useState(0);
-  const [activeCarouselBtn, setActiveCarouselBtn] = useState("active");
-
   return (
     <section className="Quote" id="interviews">
-      <div className="open-and-quote">
+      <div
+        className="carousel slide open-and-quote"
+        id="carouselExampleIndicators"
+      >
         <div className="open-quotation">,,</div>
-        <div className="main-quote">
-          {QUOTES.map((quote) => {
-            return (
-              <div className="quote-carousel-item" key={quote.id}>
-                <p className="quote-content">{quote.content}</p>
-                <p className="quote-author">
-                  <code>&#8212;</code> {quote.author}
-                </p>
-                <small className="quote-author-info">{quote.author_info}</small>
-              </div>
-            );
-          })}
+        <div className="main-quote carousel-inner">
+          <div
+            className="carousel-item quote-carousel-item active"
+            key={QUOTES[0].id}
+          >
+            <p className="quote-content">{QUOTES[0].content}</p>
+            <p className="quote-author">
+              <code>&#8212;</code> {QUOTES[0].author}
+            </p>
+            <small className="quote-author-info">{QUOTES[0].author_info}</small>
+          </div>
+          <div className="carousel-item quote-carousel-item" key={QUOTES[1].id}>
+            <p className="quote-content">{QUOTES[1].content}</p>
+            <p className="quote-author">
+              <code>&#8212;</code> {QUOTES[1].author}
+            </p>
+            <small className="quote-author-info">{QUOTES[1].author_info}</small>
+          </div>
+          <div className="carousel-item quote-carousel-item" key={QUOTES[2].id}>
+            <p className="quote-content">{QUOTES[2].content}</p>
+            <p className="quote-author">
+              <code>&#8212;</code> {QUOTES[2].author}
+            </p>
+            <small className="quote-author-info">{QUOTES[2].author_info}</small>
+          </div>
         </div>
-        <div className="quote-carousel">
-          {QUOTES.map((quote) => {
-            return (
-              <label>
-                <input type="radio" name="slider"></input>
-                <span></span>
-              </label>
-            );
-          })}
+        <div className="carousel-indicators quote-carousel">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carouselExampleIndicators"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
         </div>
         <div className="close-quotation">,,</div>
       </div>
--- src/constants/quotesInfo.js
+++ src/constants/quotesInfo.js
@@ -1,10 +1,23 @@
-const QUOTES = [
+export const QUOTES = [
   {
+    id: 0,
     content:
-      "For COVID I say, I got it and I was fine with it. We've had a good experience when I personalize it with me and my family, This vaccine is really safe.",
+      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
+    author: "Dr. Robert Huffard, MD",
+    author_info: "Pediatrics specialist in State College, PA",
+  },
+  {
+    id: 1,
+    content:
+      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
+    author: "Dr. Robert Huffard, MD",
+    author_info: "Pediatrics specialist in State College, PA",
+  },
+  {
+    id: 2,
+    content:
+      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
     author: "Dr. Robert Huffard, MD",
     author_info: "Pediatrics specialist in State College, PA",
   },
 ];
-
-module.exports = { QUOTES };
