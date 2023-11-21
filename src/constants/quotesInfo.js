2e4e9802bebc0f1b1de7c714e811abeb84f9e0fa
Dakota Fabro <dakota@weareoutright.com>
2023-11-21 12:48:05 -0800

    Updated quotes

--- src/constants/quotesInfo.js
+++ src/constants/quotesInfo.js
@@ -2,22 +2,22 @@ export const QUOTES = [
   {
     id: 0,
     content:
-      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
+      "As a pediatrician I know the importance of vaccines. As a mother, it was important for me to make sure my baby is protected.",
+    author: "Dr. Stephanie Lee",
+    author_info: "General Pediatrician, Reading, PA",
   },
   {
     id: 1,
     content:
-      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
+      "It’s very for parents to protect their kids from COVID and prevent some of those severe complications.",
+    author: "Dr. Nitin Humar",
+    author_info: "Lehigh Valley Hospitals",
   },
   {
     id: 2,
     content:
-      "For COVID I say, I got it and I was fine with it. We’ve had a good experience when I personalize it with me and my family, This vaccine is really safe.",
-    author: "Dr. Robert Huffard, MD",
-    author_info: "Pediatrics specialist in State College, PA",
+      "We know that this vaccination is safe and effective. It prevent kids from getting hospitalized from the disease.",
+    author: "Dr. Norell Atkinson",
+    author_info: "St. Christopher’s Hospital for Children",
   },
 ];
