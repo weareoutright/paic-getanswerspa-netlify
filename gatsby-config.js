0ebf805e8a54e4736418ad9e424af0c7d97dd966
Dakota Fabro <dakota@weareoutright.com>
2023-11-28 14:05:57 -0800

    Added gtag for GA in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
-          "GA-TRACKING_ID", // Google Analytics / GA
+          "G-RGYCDXQMSF", // Google Analytics / GA
           "AW-CONVERSION_ID", // Google Ads / Adwords / AW
           "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
         ],
@@ -31,7 +31,7 @@ module.exports = {
         // This object is used for configuration specific to this plugin
         pluginConfig: {
           // Puts tracking script in the head instead of the body
-          head: false,
+          head: true,
           // Setting this parameter is also optional
           respectDNT: true,
           // Avoids sending pageview hits from custom paths
